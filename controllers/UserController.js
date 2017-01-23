19de10fa034902b2a8eb86a26079d2b02e3806c5
Sebastian Radloff <sradloff23@gmail.com>
2017-01-23 18:36:28 -0500

    Refactored postgresql configuration out of the UserController and into its own file.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,22 +1,11 @@
 import express from 'express';
 import HTTPStatus from 'http-status';
+import PgClient from '../PgClientConfig';
 
 import UserService from '../services/UserService';
 import UserRepository from '../repositories/UserRepository';
 
-import pg from 'pg';
-
-const Client = pg.Client;
-const DATABASE_URL = process.env.DATABASE_URL || "postgresql://localhost/test_db";
-// needed for heroku
-// pg.defaults.ssl = true;
-
-const PG_CLIENT = new Client(DATABASE_URL);
-//disconnect client when all queries are finished
-PG_CLIENT.on('drain', PG_CLIENT.end.bind(PG_CLIENT));
-PG_CLIENT.connect();
-
-const userRepository = new UserRepository(PG_CLIENT);
+const userRepository = new UserRepository(PgClient);
 const userService = new UserService(userRepository);
 
 const UserController = express();
