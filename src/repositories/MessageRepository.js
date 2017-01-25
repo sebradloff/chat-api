19f7558458bfd96ff7d79e9ee0d8caac47d1db06
Sebastian Radloff <sradloff23@gmail.com>
2017-01-24 21:31:29 -0500

    Creates MessageRepository#getAllMessagesBetweenTwoUsers.

--- src/repositories/MessageRepository.js
+++ src/repositories/MessageRepository.js
@@ -25,6 +25,27 @@ class MessageRepository {
     });
   }
 
+  getAllMessagesBetweenTwoUsers(userId1, userId2) {
+    return new Promise((resolve, reject) => {
+      this.pool.connect((err, client, done) => {
+        if (err) reject('error fetching client from pool', err);
+
+        client.query('SELECT chat_api.message.id, chat_api.message.contents, chat_api.message.receiver_id, ' +
+                     'chat_api.message.sender_id, chat_api.message.date_sent ' +
+                     'FROM chat_api.message ' +
+                     'WHERE (chat_api.message.sender_id = $1 AND chat_api.message.receiver_id = $2) ' +
+                     'OR (chat_api.message.sender_id = $2 AND chat_api.message.receiver_id = $1) ' +
+                     'ORDER BY chat_api.message.date_sent DESC;',
+                     [userId1, userId2],
+                     (error, result) => {
+                       if (error) reject(error);
+                       resolve(result);
+                       done();
+                     });
+      });
+    });
+  }
+
 }
 
 export default MessageRepository;
--- test/repositories/MessageRepository.spec.js
+++ test/repositories/MessageRepository.spec.js
@@ -1,5 +1,9 @@
 import pg from 'pg';
+import moment from 'moment';
+
 import PgPool from '../../src/PgPoolConfig';
+import MessageBuilder from '../utils/MessageBuilder';
+import UserBuilder from '../utils/UserBuilder';
 
 import sinon from 'sinon';
 
@@ -11,9 +15,13 @@ chai.use(sinonChai);
 import MessageRepository from '../../src/repositories/MessageRepository';
 
 describe('MessageRepository', () => {
-  let messageRepository;
+  let messageRepository, messageBuilder;
   beforeEach(() => {
     messageRepository = new MessageRepository(PgPool);
+    messageBuilder = new MessageBuilder();
+  });
+  afterEach(() => {
+    messageBuilder.tearDown();
   });
   describe('#createMessage', () => {
     it('should successfully create a message and return the message id, sender_id, receiver_id, date_sent, and contents', (done) => {
@@ -25,6 +33,36 @@ describe('MessageRepository', () => {
         .then((result) => {
           result.rowCount.should.equal(1);
           result.rows[0].should.include.keys('id', 'sender_id', 'receiver_id', 'date_sent', 'contents');
+          done();
+        });
+    });
+  });
+  describe('#getAllMessagesBetweenTwoUsers', () => {
+    let message1, message2, message3, userId1, userId2;
+    beforeEach(() => {
+      userId1 = '2c3921d7-2465-4025-983f-6552bb19c855';
+      userId2 = 'af25e231-3ad8-44ab-a744-5594c484bf23';
+      message1 = { contents: 'What are you up to?', sender_id: userId1, receiver_id: userId2 };
+      message2 = { contents: 'Nothing much man. Hbu?', sender_id: userId2, receiver_id: userId1 };
+      message3 = { contents: 'I have other friends', sender_id: userId1 };
+      return (
+        messageBuilder.createMessage(message1)
+          .then(() => {
+            // delay is needed to fake the time stamp
+            return setTimeout(messageBuilder.createMessage(message2), 1000);
+          })
+          .then(() => {
+            return messageBuilder.createMessage(message3);
+          })
+      );
+    });
+    it('should return two messages that were sent between the users, from least recent to most recent', (done) => {
+      messageRepository.getAllMessagesBetweenTwoUsers(userId1, userId2)
+        .then((result) => {
+          result.rowCount.should.equal(2);
+          result.rows[0].should.include.keys('id', 'sender_id', 'receiver_id', 'date_sent', 'contents');
+          result.rows[0].contents.should.equal(message1.contents);
+          result.rows[1].contents.should.equal(message2.contents);
           done();
         });
     });
