0dd926f64f501c5006d584ea69a47c07d9432ac5
Sebastian Radloff <sradloff23@gmail.com>
2017-01-25 00:13:20 -0500

    Adds documentation for UserController routes.

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -11,6 +11,56 @@ const userService = new UserService(userRepository);
 
 const UserController = express();
 
+/**
+ * @swagger
+ *   parameters:
+ *     user-id:
+ *       name: user-id
+ *       in: path
+ *       description: The user uuid
+ *       type: string
+ *       required: true
+ */
+
+/**
+ * @swagger
+ * definition:
+ *   Error:
+ *     type: object
+ */
+
+/**
+ * @swagger
+ * definition:
+ *   User:
+ *     properties:
+ *       name:
+ *         type: string
+ *       id:
+ *         type: string
+ */
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     tags:
+ *       - Users
+ *     description: Returns all users
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of users
+ *         schema:
+ *           type: array
+ *           items:
+ *             $ref: '#/definitions/User'
+ *       400:
+ *         description: Unexpected error
+ *         schema:
+ *             $ref: '#/definitions/Error'
+ */
 UserController.get('/', (req, res) => {
   userService.getAllUsers()
     .then((responseObject) => {
@@ -19,6 +69,28 @@ UserController.get('/', (req, res) => {
     .catch(error => res.status(HTTPStatus.BAD_REQUEST).send({ error }));
 });
 
+/**
+ * @swagger
+ * /users/{user-id}:
+ *   parameters:
+ *     - $ref: '#/parameters/user-id'
+ *   get:
+ *     tags:
+ *       - Users
+ *     description: Returns a particular user
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Get a single user
+ *         schema:
+ *             $ref: '#/definitions/User'
+ *       400:
+ *         description: Unexpected error
+ *         schema:
+ *             $ref: '#/definitions/Error'
+ */
+
 UserController.get('/:id', (req, res) => {
   userService.getUser(req.params.id)
     .then((responseObject) => {
@@ -26,6 +98,32 @@ UserController.get('/:id', (req, res) => {
     })
     .catch(error => res.status(HTTPStatus.BAD_REQUEST).send({ error }));
 });
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     tags:
+ *       - Users
+ *     description: Create a new user
+ *     parameters:
+ *       - name: name
+ *         in: body
+ *         description: The name of the user
+ *         schema:
+ *           type: string
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       201:
+ *         description: Created a user
+ *         schema:
+ *             $ref: '#/definitions/User'
+ *       400:
+ *         description: Unexpected error
+ *         schema:
+ *             $ref: '#/definitions/Error'
+ */
 
 UserController.post('/', (req, res) => {
   logger.log('info', `create user request with payload ${JSON.stringify(req.body)}`);
