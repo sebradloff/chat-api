cbbf4496abb298fd7012a1010627a4d796fb4dde
Sebastian Radloff <sradloff23@gmail.com>
2017-01-24 22:09:21 -0500

    Adds ability to get all messages that were sent between two users.

--- src/controllers/MessageController.js
+++ src/controllers/MessageController.js
@@ -10,6 +10,15 @@ const messageService = new MessageService(messageRepository);
 
 const MessageController = express();
 
+MessageController.get('/users/:userId1/users/:userId2', (req, res) => {
+  const { userId1, userId2 } = req.params;
+  messageService.getAllMessagesBetweenTwoUsers(userId1, userId2)
+    .then((responseObject) => {
+      res.status(HTTPStatus.OK).send(responseObject);
+    })
+    .catch(error => res.status(HTTPStatus.BAD_REQUEST).send({ error }));
+});
+
 MessageController.post('/', (req, res) => {
   messageService.createMessage(req.body)
     .then((responseObject) => {
--- test/controllers/MessageController.spec.js
+++ test/controllers/MessageController.spec.js
@@ -1,5 +1,6 @@
 import supertest from 'supertest';
 import express from 'express';
+import MessageBuilder from '../utils/MessageBuilder';
 
 import chai from 'chai';
 chai.should();
@@ -24,6 +25,38 @@ describe('MessageController', () => {
           res.body.sender_id.should.equal(sender_id);
           res.body.receiver_id.should.equal(receiver_id);
           res.body.contents.should.equal(contents);
+          done();
+        });
+    });
+  });
+  describe('get to /messages/users/:userId1/users/:userId2', () => {
+    let messageBuilder, userId1, userId2;
+    beforeEach(() => {
+      messageBuilder = new MessageBuilder();
+      userId1 = '2c3921d7-2465-4025-983f-6552bb19c855';
+      userId2 = 'af25e231-3ad8-44ab-a744-5594c484bf23';
+      const message1 = { contents: 'What are you up to?', sender_id: userId1, receiver_id: userId2 };
+      const message2 = { contents: 'Nothing much man. Hbu?', sender_id: userId2, receiver_id: userId1 };
+      return (
+        messageBuilder.createMessage(message1)
+          .then(() => {
+            messageBuilder.createMessage(message2);
+          })
+      );
+    });
+    afterEach(() => {
+      messageBuilder.tearDown();
+    });
+    it('should get all 2 messages sent between those users', (done) => {
+      const url = encodeURI(`/messages/users/${userId1}/users/${userId2}`);
+      app
+        .get(url)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+          res.body.should.include.keys('userId1', 'userId2', 'messages');
+          res.body.messages.length.should.equal(2);
           done();
         });
     });
